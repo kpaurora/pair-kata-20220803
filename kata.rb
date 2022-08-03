7ac4d1eff3653d4983009636575d6f6517cb0c8c
Kamil Politowicz <kpolitowicz@aurorasolar.com>
2022-08-03 16:53:21 -0400

    Post-session state
    Squashed commit of the following:
    
    commit 3c322ffb3cc8070acae0cc38c8ac38359d0bbe5a
    Author: Kamil Politowicz <kpolitowicz@aurorasolar.com>
    Date:   Wed Aug 3 16:52:58 2022 -0400
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:kata.rb
    
    commit af6821f2fafa0ce2f6cb2804bf4e980c7f0e7259
    Author: Connor Moore <cmoore@aurorasolar.com>
    Date:   Wed Aug 3 13:21:46 2022 -0700
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:.idea/workspace.xml
    
    commit 353bfbb8ac83e0fd60c83b7b82e6ab5d41f3dcfa
    Author: Kamil Politowicz <kpolitowicz@aurorasolar.com>
    Date:   Wed Aug 3 16:14:11 2022 -0400
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:kata.rb
    
    commit 9c290bbd8d2d0f761d402086af947d5794908cfa
    Author: Connor Moore <cmoore@aurorasolar.com>
    Date:   Wed Aug 3 13:06:26 2022 -0700
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:kata_spec.rb
    
    commit dc95aaf50ab8c8a04cc656ab585ce203340b1a60
    Author: Kamil Politowicz <kpolitowicz@aurorasolar.com>
    Date:   Wed Aug 3 15:59:01 2022 -0400
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:kata_spec.rb
    
    commit 4c7cf6022cf9861b48d4d82631ae3048bbf7eb03
    Author: Connor Moore <cmoore@aurorasolar.com>
    Date:   Wed Aug 3 12:50:33 2022 -0700
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:.idea/workspace.xml
    
    commit 4f0bd2a22f6cfca55cbddc1589856e7c5859fc65
    Author: Connor Moore <cmoore@aurorasolar.com>
    Date:   Wed Aug 3 12:49:17 2022 -0700
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:.idea/workspace.xml
    
    commit f292bccf3b87b998051876baa4c62147bc246145
    Author: Kamil Politowicz <kpolitowicz@aurorasolar.com>
    Date:   Wed Aug 3 15:40:21 2022 -0400
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:kata.rb
    
    Co-authored-by: Connor Moore <cmoore@aurorasolar.com>

--- kata.rb
+++ kata.rb
@@ -1,5 +1,49 @@
+require "set"
+
 class Kata
-  def do_something(param)
-    param * 2
+  class RangeSet
+    def initialize
+      @range_set = []
+    end
+
+    def +(range)
+      # @range_set << range
+      @range_set.each do |rng|
+      end
+      
+      self
+    end
+
+    def range_size
+      sum = 0
+      @range_set.each do |range|
+        min = range.min
+        max = range.max
+        sum += max - min 
+      end
+      sum
+    end
+  end
+
+  def interval_sum(intervals)
+    range_set = RangeSet.new
+    intervals.each do |interval|
+      range_set = range_set + interval
+    end
+    puts "HERE: #{range_set}"
+    range_set.range_size
+    # total_range = Set.new
+    # intervals.each do |interval|
+    #   min = interval.min
+    #   max = interval.max
+    #   r = Range.new(min, max - 1)
+    #   total_range += r.to_set
+    # end
+    # total_range.size
   end
 end
+
+##############################################################################
+    #################
+               ######################
+                                                   ##################
