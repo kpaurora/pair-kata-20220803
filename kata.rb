b1298168eaa489d88d9ef2aede62149303abad84
Kamil Politowicz <kpolitowicz@aurorasolar.com>
2022-08-31 16:29:19 -0400

    removed old kata

--- kata.rb
+++ kata.rb
@@ -1,49 +1,4 @@
 require "set"
 
 class Kata
-  class RangeSet
-    def initialize
-      @range_set = []
-    end
-
-    def +(range)
-      # @range_set << range
-      @range_set.each do |rng|
-      end
-      
-      self
-    end
-
-    def range_size
-      sum = 0
-      @range_set.each do |range|
-        min = range.min
-        max = range.max
-        sum += max - min 
-      end
-      sum
-    end
-  end
-
-  def interval_sum(intervals)
-    range_set = RangeSet.new
-    intervals.each do |interval|
-      range_set = range_set + interval
-    end
-    puts "HERE: #{range_set}"
-    range_set.range_size
-    # total_range = Set.new
-    # intervals.each do |interval|
-    #   min = interval.min
-    #   max = interval.max
-    #   r = Range.new(min, max - 1)
-    #   total_range += r.to_set
-    # end
-    # total_range.size
-  end
 end
-
-##############################################################################
-    #################
-               ######################
-                                                   ##################
--- kata_spec.rb
+++ kata_spec.rb
@@ -2,51 +2,5 @@ require "./kata"
 
 RSpec.describe Kata do
   describe "when an empty array is passed" do
-    it "returns 0" do
-      kata = Kata.new
-      expect(kata.interval_sum([])).to eq(0)
-    end
-  end
-
-  describe "when a single interval is passed" do
-    it "returns length of that interval" do
-      kata = Kata.new
-      expect(kata.interval_sum([[-1, 5]])).to eq(6)
-    end
-  end
-
-  describe "when three non-overlapping intervals are passed" do
-    it "returns sum of lengths" do
-      kata = Kata.new
-      expect(kata.interval_sum([[-1, 5], [5, 6], [9, 20]])).to eq(18)
-    end
-  end
-
-  describe "when two overlapping intervals are passed" do
-    it "returns the length of the combined interval" do
-      kata = Kata.new
-      expect(kata.interval_sum([[-1, 5], [3, 7]])).to eq(8)
-    end
-  end
-
-  describe "when mix of overlapping and non-overlapping intervals are passed" do
-    it "returns the length of the combined intervals" do
-      kata = Kata.new
-      expect(kata.interval_sum([[-1, 5], [3, 7], [9, 10], [15, 20], [17, 18]])).to eq(14)
-    end
-  end
-
-  describe "when interval with last element smaller than first is passed" do
-    it "returns the length of the interval" do
-      kata = Kata.new
-      expect(kata.interval_sum([[5, -1]])).to eq(6)
-    end
-  end
-
-  describe "when interval with real number bounds is passed" do
-    it "returns the real number length of the interval" do
-      kata = Kata.new
-      expect(kata.interval_sum([[-0.5, 0.75]])).to eq(1.25)
-    end
   end
 end
