b398821bf89ae0e031f5804eb581404078ecedaa
Kamil Politowicz <kpolitowicz@aurorasolar.com>
2022-08-31 17:31:45 -0400

    session Liam + Kamil Squashed commit of the following:
    commit ac1045b4f85ff07ecd39be7d2b2716ac2b5aaa39
    Author: Kamil Politowicz <kpolitowicz@aurorasolar.com>
    Date:   Wed Aug 31 17:31:40 2022 -0400
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:kata.rb
    
    commit 4ff2c3db0618d3b6590d1403d97278f1f82483bf
    Author: lworkman <liam.workman@gmail.com>
    Date:   Wed Aug 31 14:26:51 2022 -0700
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:kata_spec.rb
    
    commit 4b01efae18f1d4430a690ed7ecdc07cf023eec1f
    Author: Kamil Politowicz <kpolitowicz@aurorasolar.com>
    Date:   Wed Aug 31 17:15:25 2022 -0400
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:kata.rb
    
    commit 0f103607fa669ccc4b9e00bf075dfe0ac13ce5b8
    Author: lworkman <liam.workman@gmail.com>
    Date:   Wed Aug 31 14:04:56 2022 -0700
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:kata_spec.rb
    
    commit 0b7cd271c21847516b190726cb50d89e0e8e6700
    Author: Kamil Politowicz <kpolitowicz@aurorasolar.com>
    Date:   Wed Aug 31 16:54:25 2022 -0400
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:kata.rb
    
    commit 6fb7f73d3700e2c07db883282e86aa1ae79ce7f4
    Author: lworkman <liam.workman@gmail.com>
    Date:   Wed Aug 31 13:44:02 2022 -0700
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:kata.rb
    
    Co-authored-by: lworkman <liam.workman@gmail.com>

--- kata.rb
+++ kata.rb
@@ -1,4 +1,37 @@
 require "set"
 
 class Kata
+
+  DEFAULT_MATCHES = {
+    ")" => "(",
+    "}" => "{",
+    "]" => "["
+  }
+
+  attr_reader :matching_chars
+  
+  def initialize(matching_chars = DEFAULT_MATCHES)
+    @matching_chars = matching_chars
+  end 
+
+  def is_balanced_braces(str)
+    stack = []
+    allowed_chars = matching_chars.to_a.flatten
+
+    str.split("").select{|c| allowed_chars.include?(c)}.each do |c|
+      if matching_chars.values.include? c
+        stack << c
+      elsif is_matching(stack.last, c)
+        stack.pop
+      else
+        return false
+      end 
+    end
+
+    stack.empty?
+  end
+
+  def is_matching(opening, closing)
+    opening == matching_chars[closing]
+  end
 end
--- kata_spec.rb
+++ kata_spec.rb
@@ -1,6 +1,69 @@
 require "./kata"
 
 RSpec.describe Kata do
-  describe "when an empty array is passed" do
+
+  describe "new" do
+    it "supports custom pairs" do
+      expect(Kata.new({
+        "A" => "a",
+        "B" => "b",
+        "C" => "c"
+      }).is_balanced_braces("aAbBcC")).to be true
+    end
+  end
+
+  describe "is_balanced_braces" do
+
+    it "returns false when leftover closing brack" do
+      expect(Kata.new.is_balanced_braces(")")).to be false
+    end
+
+    it "returns true when passed in matching ()" do
+      expect(Kata.new.is_balanced_braces("()")).to be true
+    end
+
+    it "returns false when passed just one (" do
+      expect(Kata.new.is_balanced_braces("(")).to be false
+    end
+
+    it "returns true when passed in matching {}" do
+      expect(Kata.new.is_balanced_braces("{}")).to be true
+    end
+
+    it "returns false when passed just one {" do
+      expect(Kata.new.is_balanced_braces("{")).to be false
+    end
+
+    it "returns false when passed in not matching {)" do
+      expect(Kata.new.is_balanced_braces("{)")).to be false
+    end
+
+    it "returns true when passed in matching []" do
+      expect(Kata.new.is_balanced_braces("[]")).to be true
+    end
+
+    it "returns false when passed just one [" do
+      expect(Kata.new.is_balanced_braces("[")).to be false
+    end
+    
+    it "returns true for complicated but valid nesting" do
+      expect(Kata.new.is_balanced_braces("(){[]}")).to be true
+    end
+
+    it "returns false for complicated but invalid nesting" do
+      expect(Kata.new.is_balanced_braces("(){[)]}")).to be false
+    end
+    
+    it "returns false for out order brackets" do
+      expect(Kata.new.is_balanced_braces("([)]")).to be false
+    end
+    
+    it "ignores non-bracket characters" do
+      expect(Kata.new.is_balanced_braces("(IGNORE_THIS_STRING)")).to be true
+    end
+    
+    it "returns true for empty string" do
+      expect(Kata.new.is_balanced_braces("")).to be true
+    end
   end
 end
